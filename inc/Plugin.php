4b88b04b54872d7cc1507b38b50e367a7c4e3ad5
badasswp <badasswpdev@gmail.com>
2024-07-09 09:46:34 +0100

    refactor: implement Logger class, move methods

--- inc/Plugin.php
+++ inc/Plugin.php
@@ -86,38 +86,7 @@ class Plugin {
 	 * @return void
 	 */
 	public function run(): void {
-		add_action( 'webp_img_convert', [ $this, 'add_webp_meta_to_attachment' ], 10, 2 );
 		add_filter( 'attachment_fields_to_edit', [ $this, 'add_webp_attachment_fields' ], 10, 2 );
-	}
-
-	/**
-	 * Add WebP meta to Attachment.
-	 *
-	 * This is responsible for creating meta data or logging errors
-	 * depending on the conversion result ($webp).
-	 *
-	 * @since 1.0.2
-	 *
-	 * @param string|\WP_Error $webp          WebP's relative path.
-	 * @param int              $attachment_id Image ID.
-	 *
-	 * @return void
-	 */
-	public function add_webp_meta_to_attachment( $webp, $attachment_id ): void {
-		if ( ! is_wp_error( $webp ) && ! get_post_meta( $attachment_id, 'webp_img', true ) ) {
-			update_post_meta( $attachment_id, 'webp_img', $webp );
-		}
-
-		if ( is_wp_error( $webp ) ) {
-			wp_insert_post(
-				[
-					'post_type'    => 'webp_error',
-					'post_title'   => 'WebP error log, ID - ' . $attachment_id,
-					'post_content' => (string) $webp->get_error_message(),
-					'post_status'  => 'publish',
-				]
-			);
-		}
 	}
 
 	/**
