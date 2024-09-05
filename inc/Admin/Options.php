264ff09b9905d0c81243aabd758fff5911a45a76
badasswp <badasswpdev@gmail.com>
2024-09-05 19:15:30 +0100

    chore: seperate form params into arrays

--- inc/Admin/Options.php
+++ inc/Admin/Options.php
@@ -12,77 +12,106 @@ namespace ImageConverterWebP\Admin;
 
 class Options {
 	/**
-	 * Form Fields.
-	 *
-	 * Store all the form values needed to generate
-	 * the form successfully.
+	 * The Form.
 	 *
 	 * @since 1.1.2
 	 */
 	public const FORM = [
-		'notice' => [
-			'label' => 'Settings Saved.',
+		'page'   => self::FORM_PAGE,
+		'notice' => self::FORM_NOTICE,
+		'fields' => self::FORM_FIELDS,
+		'submit' => self::FORM_SUBMIT,
+	];
+
+	/**
+	 * Form Page.
+	 *
+	 * @since 1.1.2
+	 */
+	public const FORM_PAGE = [
+		'title'   => 'Image Converter for WebP',
+		'summary' => 'Convert your WordPress JPG/PNG images to WebP formats during runtime.',
+	];
+
+	/**
+	 * Form Submit.
+	 *
+	 * @since 1.1.2
+	 */
+	public const FORM_SUBMIT = [
+		'heading' => 'Actions',
+		'button'  => [
+			'name'  => 'icfw_save_settings',
+			'label' => 'Save Changes',
 		],
-		'submit' => [
-			'heading' => 'Actions',
-			'button'  => [
-				'name'  => 'icfw_save_settings',
-				'label' => 'Save Changes',
-			],
-			'nonce'   => [
-				'name'   => 'icfw_settings_nonce',
-				'action' => 'icfw_settings_action',
-			],
+		'nonce'   => [
+			'name'   => 'icfw_settings_nonce',
+			'action' => 'icfw_settings_action',
 		],
-		'fields' => [
-			'icfw_conv_options' => [
-				'heading'  => 'Conversion Options',
-				'controls' => [
-					'quality'   => [
-						'control'     => 'text',
-						'placeholder' => '50',
-						'label'       => 'Conversion Quality',
-						'summary'     => 'e.g. 75',
-					],
-					'converter' => [
-						'control' => 'select',
-						'label'   => 'WebP Engine',
-						'summary' => 'e.g. Imagick',
-						'options' => [
-							'gd'      => 'GD',
-							'cwebp'   => 'CWebP',
-							'ffmpeg'  => 'FFMPeg',
-							'imagick' => 'Imagick',
-							'gmagick' => 'Gmagick',
-						],
-					],
+	];
+
+	/**
+	 * Form Fields.
+	 *
+	 * @since 1.1.2
+	 */
+	public const FORM_FIELDS = [
+		'icfw_conv_options' => [
+			'heading'  => 'Conversion Options',
+			'controls' => [
+				'quality'   => [
+					'control'     => 'text',
+					'placeholder' => '50',
+					'label'       => 'Conversion Quality',
+					'summary'     => 'e.g. 75',
 				],
-			],
-			'icfw_img_options'  => [
-				'heading'  => 'Image Options',
-				'controls' => [
-					'upload'    => [
-						'control' => 'checkbox',
-						'label'   => 'Convert Images on Upload',
-						'summary' => 'This is useful for new images.',
-					],
-					'page_load' => [
-						'control' => 'checkbox',
-						'label'   => 'Convert Images on Page Load',
-						'summary' => 'This is useful for existing images.',
-					],
-				],
-			],
-			'icfw_log_options'  => [
-				'heading'  => 'Log Options',
-				'controls' => [
-					'logs' => [
-						'control' => 'checkbox',
-						'label'   => 'Log errors for Failed Conversions',
-						'summary' => 'Enable this option to log errors.',
+				'converter' => [
+					'control' => 'select',
+					'label'   => 'WebP Engine',
+					'summary' => 'e.g. Imagick',
+					'options' => [
+						'gd'      => 'GD',
+						'cwebp'   => 'CWebP',
+						'ffmpeg'  => 'FFMPeg',
+						'imagick' => 'Imagick',
+						'gmagick' => 'Gmagick',
 					],
 				],
 			],
 		],
+		'icfw_img_options'  => [
+			'heading'  => 'Image Options',
+			'controls' => [
+				'upload'    => [
+					'control' => 'checkbox',
+					'label'   => 'Convert Images on Upload',
+					'summary' => 'This is useful for new images.',
+				],
+				'page_load' => [
+					'control' => 'checkbox',
+					'label'   => 'Convert Images on Page Load',
+					'summary' => 'This is useful for existing images.',
+				],
+			],
+		],
+		'icfw_log_options'  => [
+			'heading'  => 'Log Options',
+			'controls' => [
+				'logs' => [
+					'control' => 'checkbox',
+					'label'   => 'Log errors for Failed Conversions',
+					'summary' => 'Enable this option to log errors.',
+				],
+			],
+		],
+	];
+
+	/**
+	 * Form Notice.
+	 *
+	 * @since 1.1.2
+	 */
+	public const FORM_NOTICE = [
+		'label' => 'Settings Saved.',
 	];
 }
