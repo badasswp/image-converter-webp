2051fd59fc7720031fc15935438bc17ebc31ef67
badasswp <badasswpdev@gmail.com>
2024-09-05 07:05:08 +0100

    chore: make form more extensible

--- inc/Admin/Options.php
+++ inc/Admin/Options.php
@@ -14,56 +14,63 @@ class Options {
 	/**
 	 * Form Fields.
 	 *
+	 * Store all the form values needed to generate
+	 * the form successfully.
+	 *
 	 * @since 1.1.2
 	 */
-	public const FIELDS = [
-		'icfw_conv_options' => [
-			'label'    => 'Conversion Options',
-			'controls' => [
-				'quality'   => [
-					'control'     => 'text',
-					'placeholder' => '50',
-					'label'       => 'Conversion Quality',
-					'summary'     => 'e.g. 75',
-				],
-				'converter' => [
-					'control' => 'select',
-					'label'   => 'WebP Engine',
-					'summary' => 'e.g. Imagick',
-					'options' => [
-						'gd'      => 'GD',
-						'cwebp'   => 'CWebP',
-						'ffmpeg'  => 'FFMPeg',
-						'imagick' => 'Imagick',
-						'gmagick' => 'Gmagick',
+	public const FORM = [
+		'submit' => [],
+		'notice' => [],
+		'fields' => [
+			'icfw_conv_options' => [
+				'label'    => 'Conversion Options',
+				'controls' => [
+					'quality'   => [
+						'control'     => 'text',
+						'placeholder' => '50',
+						'label'       => 'Conversion Quality',
+						'summary'     => 'e.g. 75',
+					],
+					'converter' => [
+						'control' => 'select',
+						'label'   => 'WebP Engine',
+						'summary' => 'e.g. Imagick',
+						'options' => [
+							'gd'      => 'GD',
+							'cwebp'   => 'CWebP',
+							'ffmpeg'  => 'FFMPeg',
+							'imagick' => 'Imagick',
+							'gmagick' => 'Gmagick',
+						],
 					],
 				],
 			],
-		],
-		'icfw_img_options'  => [
-			'label'    => 'Image Options',
-			'controls' => [
-				'upload'    => [
-					'control' => 'checkbox',
-					'label'   => 'Convert Images on Upload',
-					'summary' => 'This is useful for new images.',
-				],
-				'page_load' => [
-					'control' => 'checkbox',
-					'label'   => 'Convert Images on Page Load',
-					'summary' => 'This is useful for existing images.',
-				],
-			],
-		],
-		'icfw_log_options'  => [
-			'label'    => 'Log Options',
-			'controls' => [
-				'logs' => [
-					'control' => 'checkbox',
-					'label'   => 'Log errors for Failed Conversions',
-					'summary' => 'Enable this option to log errors.',
+			'icfw_img_options'  => [
+				'label'    => 'Image Options',
+				'controls' => [
+					'upload'    => [
+						'control' => 'checkbox',
+						'label'   => 'Convert Images on Upload',
+						'summary' => 'This is useful for new images.',
+					],
+					'page_load' => [
+						'control' => 'checkbox',
+						'label'   => 'Convert Images on Page Load',
+						'summary' => 'This is useful for existing images.',
+					],
 				],
 			],
-		],
+			'icfw_log_options'  => [
+				'label'    => 'Log Options',
+				'controls' => [
+					'logs' => [
+						'control' => 'checkbox',
+						'label'   => 'Log errors for Failed Conversions',
+						'summary' => 'Enable this option to log errors.',
+					],
+				],
+			],
+		]
 	];
 }
