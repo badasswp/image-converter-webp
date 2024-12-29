0aea86d5bebb3490225012ef2cf54b6c762e0e4d
badasswp <badasswpdev@gmail.com>
2024-12-29 16:15:53 +0100

    chore: add missing covers tag

--- tests/Services/MainTest.php
+++ tests/Services/MainTest.php
@@ -15,6 +15,7 @@ use ImageConverterWebP\Core\Converter;
  * @covers \ImageConverterWebP\Services\Main::register_webp_img_srcset_creation
  * @covers \ImageConverterWebP\Services\Main::register_webp_img_deletion
  * @covers \ImageConverterWebP\Services\Main::register_webp_attachment_fields
+ * @covers \ImageConverterWebP\Services\Main::show_webp_images_on_wp_media_modal
  * @covers icfw_get_settings
  */
 class MainTest extends TestCase {
