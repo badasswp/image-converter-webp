dcdf3b5894b556b9d7e787fa9cbc4be632a6ac9e
badasswp <badasswpdev@gmail.com>
2024-12-29 23:35:28 +0100

    test: add tests for get equivalent

--- tests/Helpers/FunctionsTest.php
+++ tests/Helpers/FunctionsTest.php
@@ -8,6 +8,7 @@ require_once __DIR__ . '/../../inc/Helpers/functions.php';
 
 /**
  * @covers icfw_get_settings
+ * @covers icfw_get_equivalent
  */
 class FunctionsTest extends TestCase {
 	public function test_icfw_get_settings() {
@@ -23,5 +24,11 @@ class FunctionsTest extends TestCase {
 		$is_image_converted_on_upload = icfw_get_settings( 'upload', [] );
 
 		$this->assertTrue( $is_image_converted_on_upload );
+	}
+
+	public function test_icfw_get_equivalent() {
+		$webp = icfw_get_equivalent( 'https://www.example.com/wp-content/uploads/sample.jpeg' );
+
+		$this->assertSame( $webp, 'https://www.example.com/wp-content/uploads/sample.webp' );
 	}
 }
