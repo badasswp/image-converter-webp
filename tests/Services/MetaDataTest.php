56c6afe6293190889d01ea498c437f77bea8daa6
badasswp <badasswpdev@gmail.com>
2025-02-14 15:50:05 +0100

    test: add tests for new method

--- tests/Services/MetaDataTest.php
+++ tests/Services/MetaDataTest.php
@@ -4,6 +4,7 @@ namespace ImageConverterWebP\Tests\Services;
 
 use Mockery;
 use WP_Mock\Tools\TestCase;
+use ImageConverterWebP\Core\Converter;
 use ImageConverterWebP\Services\MetaData;
 
 /**
@@ -29,7 +30,7 @@ class MetaDataTest extends TestCase {
 
 	public function test_register() {
 		\WP_Mock::expectActionAdded( 'icfw_convert', [ $this->metadata, 'add_webp_meta_to_attachment' ], 10, 2 );
-		\WP_Mock::expectActionAdded( 'icfw_convert', [ $this->metadata, 'add_webp_for_scaled_image' ], 10, 2 );
+		\WP_Mock::expectActionAdded( 'icfw_convert', [ $this->metadata, 'add_webp_for_scaled_images' ], 10, 2 );
 
 		$this->metadata->register();
 
@@ -76,6 +77,67 @@ class MetaDataTest extends TestCase {
 			->andReturn( null );
 
 		$this->metadata->add_webp_meta_to_attachment( $webp, 1 );
+
+		$this->assertConditionsMet();
+	}
+
+	public function test_add_webp_for_scaled_images_bails_out_if_is_wp_error() {
+		$wp_error = Mockery::mock( \WP_Error::class )->makePartial();
+		$wp_error->shouldAllowMockingProtectedMethods();
+
+		\WP_Mock::userFunction( 'is_wp_error' )
+			->andReturnUsing(
+				function ( $arg ) {
+					return $arg instanceof \WP_Error;
+				}
+			);
+
+		$this->metadata->add_webp_for_scaled_images( $wp_error, 1 );
+
+		$this->assertConditionsMet();
+	}
+
+	public function test_add_webp_for_scaled_images_bails_out_if_image_is_not_scaled() {
+		$webp = 'https://example.com/wp-content/uploads/2024/01/sample.webp';
+
+		\WP_Mock::userFunction( 'is_wp_error' )
+			->andReturnUsing(
+				function ( $arg ) {
+					return $arg instanceof \WP_Error;
+				}
+			);
+
+		\WP_Mock::userFunction( 'wp_get_attachment_url' )
+			->once()
+			->with( 1 )
+			->andReturn( 'https://example.com/wp-content/uploads/2024/01/sample.jpg' );
+
+		$this->metadata->add_webp_for_scaled_images( $webp, 1 );
+
+		$this->assertConditionsMet();
+	}
+
+	public function test_add_webp_for_scaled_images_passes() {
+		$webp = 'https://example.com/wp-content/uploads/2024/01/sample.webp';
+
+		\WP_Mock::userFunction( 'is_wp_error' )
+			->andReturnUsing(
+				function ( $arg ) {
+					return $arg instanceof \WP_Error;
+				}
+			);
+
+		\WP_Mock::userFunction( 'wp_get_attachment_url' )
+			->once()
+			->with( 1 )
+			->andReturn( 'https://example.com/wp-content/uploads/2024/01/sample-scaled.jpg' );
+
+		$this->metadata->converter = Mockery::mock( Converter::class )->makePartial();
+		$this->metadata->converter->shouldAllowMockingProtectedMethods();
+
+		$this->metadata->converter->shouldReceive( 'convert' );
+
+		$this->metadata->add_webp_for_scaled_images( $webp, 1 );
 
 		$this->assertConditionsMet();
 	}
